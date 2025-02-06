71d1bee66437aed7759331cf01e0d329e8f5da04
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-02-06 13:00:32 -0500

    Routes in individualk files for modularity

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,4 +1,4 @@
-const db = require('../connection');
+const db = require('../database');
 
 const getUsers = () => {
   return db.query('SELECT * FROM users;')
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,45 +1,14 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 
-
-
-
-//*** 5 GET routes total with homepage which isb in server.js***/
-router.post('/start_quiz', (req, res) => {
-
-  res.redirect('/quiz'); //when submot is hit, user goes to the quiz page
-})
-
-router.get('/quiz', (req, res) => {
-  res.send('Single quiz here') //will be replaced with res.render(quiz)
+router.post('/', (req, res) => {
+  res.redirect('/quiz'); 
 });
 
 
-router.get('/create', (req, res) => {
-  res.send('create here'); //will be replaced with res.render(create)
-});
-
-router.get('/results', (req, res) => {
-  res.send('results are here'); //will be replaced with res.render(results)
-});
-
-router.post('/results', (req, res) =>{ //If the user wants to go back to results, or homepage, res.redirect respectively
-  // ******** something like**********
-   //const goBack = req.body.goBack
-
-   //if(goBack === 'backtoresults'){ ***plug in HTML value from ejs***
-   // res.redirect('/results')
-   //}else if(req.body.goBack === 'backhome'){ ***plug in HTML value from ejs***
-    //res.redirect('/')
-   //}
-
-
-})
-
-router.get('/public', (req, res) => {
-  res.send ("all public quizzes here"); //will be replaced with res.render(public)
+router.get('/', (req, res) => {
+  res.render('quiz'); 
 });
 
 module.exports = router;
-
