2c8ad253857eb07425f78c1fc8380cb6eba1dc44
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-02-07 12:22:11 -0500

    Commit before pull

--- routes/create.js
+++ routes/create.js
@@ -3,32 +3,17 @@ const router = express.Router();
 const db = require('../db/database');
 const { addQuiz } = require('../db/database');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { 
   console.log('/create triggered')
 
-  res.render('create');
+  res.render('create'); 
 });
 
 router.post('/', (req, res) =>{
-  //const {quizTitle, privacySetting, questions} = req.body;
-  //const quiz = {
-   // title: quizTitle,
-    //privacy_setting: privacySetting,
-   // url: generateUrl(),
-   // timestamnp: Date.now(),
-    //number_of_questions: questions.length,
-
-  }
-
-  // addQuiz(quiz)
-  //   .then((createQuiz) => {
-
-  //   })
 
 
-
-    //res.redirect('/create')
-)
+    res.redirect('/create')
+})
 
 router.post('/create:id', (req, res) => {
 
--- server.js
+++ server.js
@@ -45,8 +45,16 @@ app.use('/public', publicRoutes);  // Public quizzes routes
 // Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
+
+
+  
   res.render('index');
 });
+
+app.post('/', (res, req) => {
+
+  res.redirect('/create')
+})
 
 
 
