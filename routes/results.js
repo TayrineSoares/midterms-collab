c5bacea2f89f87056ac656b304b77baffc77ef20
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-02-09 14:45:10 -0500

    /results ruote done. commit before pull

--- routes/results.js
+++ routes/results.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getQuizById } = require('../db/database');
+const {getAttemptDetails } = require('../db/database');
 
 
 router.get('/', (req, res) => {
@@ -8,28 +8,26 @@ router.get('/', (req, res) => {
 
 
 });
-router.get('/:id', (req, res) => {
-  const quizId = req.params.id; 
+router.get('/:attemptId', (req, res) => {
+  const attemptId = req.params.attemptId; 
 
-  getQuizById(quizId)
-    .then((quiz) => {
-      if (!quiz) {
-        return res.status(404).send('Quiz not found'); 
+  getAttemptDetails(attemptId)
+    .then((attempt) => {
+      if (!attempt) {
+        return res.status(404).send('No attempts found');
       }
-      res.render('results', { 
-        quiz,
-        questions,
-        answers,
-        title: quiz.title }); 
+
+      res.render('results', {
+        title: attempt.quiz_title,
+        correctAnswersCount: attempt.score
+      });
     })
     .catch((err) => {
-      console.error('Error fetching quiz:', err.message);
-      res.status(500).send('Server error'); 
+      console.error('Error retrieving quiz attempt results:', err.message);
+      res.status(500).send('Server error');
     });
 });
 
-router.post('/results', (req, res) => {
-  res.render('results'); 
-});
+
 
 module.exports = router;
